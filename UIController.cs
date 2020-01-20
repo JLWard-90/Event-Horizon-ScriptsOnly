e1f2a57eb3fc2a084264e95cbfd4b5d955a5f835
JLWard-90 <53184853+JLWard-90@users.noreply.github.com>
2020-01-20 17:07:46 +0100

    Update to latest
    Updated the scripts only repo with the latest version from the main EventHorizon repo.
    
    Changes are update to UIController to use a graphical speed gauge to replace existing text.

--- UIController.cs
+++ UIController.cs
@@ -18,7 +18,12 @@ public class UIController : MonoBehaviour
     int fuelPerGaugeMarker; //This is the amount of fuel that corresponds to each section of the fuel gauge
     [SerializeField]
     GameObject[] fuelGauge; //This array contains each of the objects that make up the fuel gauge in order that they are filled up.
-    private void Start() 
+    [SerializeField]
+    int speedPerGaugeMarker = 1;
+    [SerializeField]
+    GameObject[] speedGauge;
+    int oldSpeedInt;
+    private void Start()
     {
         playerTransform = GameObject.Find("Player").transform;
         initPlayerPosition = playerTransform.position.x;
@@ -26,22 +31,25 @@ public class UIController : MonoBehaviour
         playerSpeed = 0;
         player = playerTransform.gameObject.GetComponent<PlayerController>();
         UpdateCoinText();
+        UpdateSpeedGauge(playerSpeed);
     }
-    private void FixedUpdate() 
+    private void FixedUpdate()
     {
-        if (playerTransform!=null)
+        if (playerTransform != null)
         {
             OnUpdateFuelText(player.fuel);
         }
-        
+
     }
     public void OnUpdateFuelText(float fuel)
     {
         int fuelint = (int)fuel;
-        playerSpeed = (playerTransform.position.x-initPlayerPosition - playerPosition) / Time.deltaTime;
-        playerPosition = (playerTransform.position.x-initPlayerPosition);
+        playerSpeed = (playerTransform.position.x - initPlayerPosition - playerPosition) / Time.deltaTime;
+        playerPosition = (playerTransform.position.x - initPlayerPosition);
+        //New string for fuelText now that the speedGauge is also added:
+        string fuelstring = string.Format("Distance: {0}", (int)playerPosition);
         //New string for fuelText now that the fuel gauge is being used instead:
-        string fuelstring = string.Format("Distance: {0}       Speed: {1}", (int)playerPosition, (int)playerSpeed);
+        //string fuelstring = string.Format("Distance: {0}       Speed: {1}", (int)playerPosition, (int)playerSpeed);
         //Below is the old string
         //string fuelstring = string.Format("Fuel: {0}       Distance: {1}       Speed: {2}",fuelint,(int)playerPosition,(int)playerSpeed);
         fuelText.text = fuelstring;
@@ -50,6 +58,11 @@ public class UIController : MonoBehaviour
         {
             //Debug.Log("updating fuel gauge");
             UpdateFuelGauge();
+        }
+        if (oldSpeedInt != (int)playerSpeed && speedGauge != null && speedGauge.Length > 0)
+        {
+            UpdateSpeedGauge(playerSpeed);
+            oldSpeedInt = (int)playerSpeed;
         }
     }
 
@@ -62,16 +75,32 @@ public class UIController : MonoBehaviour
     public void UpdateFuelGauge()
     {
         float playerFuel = player.fuel;
-        int nMarkers = (int) (playerFuel / fuelPerGaugeMarker);
-        for (int i=0; i<nMarkers; i++)
+        int nMarkers = (int)(playerFuel / fuelPerGaugeMarker);
+        for (int i = 0; i < nMarkers; i++)
         {
             //fuelGauge[i].SetActive(true);
             fuelGauge[i].GetComponent<Image>().enabled = true;
         }
-        for (int i=nMarkers; i<fuelGauge.Length; i++)
+        for (int i = nMarkers; i < fuelGauge.Length; i++)
         {
             //fuelGauge[i].SetActive(false);
             fuelGauge[i].GetComponent<Image>().enabled = false;
+        }
+    }
+
+    public void UpdateSpeedGauge(float playerSpeed)
+    {
+        //playerSpeed = (playerTransform.position.x - initPlayerPosition - playerPosition) / Time.deltaTime;
+        int nMarkers = (int)(playerSpeed / speedPerGaugeMarker);
+        for (int i = 0; i < nMarkers; i++)
+        {
+            //fuelGauge[i].SetActive(true);
+            speedGauge[i].GetComponent<Image>().enabled = true;
+        }
+        for (int i = nMarkers; i < speedGauge.Length; i++)
+        {
+            //fuelGauge[i].SetActive(false);
+            speedGauge[i].GetComponent<Image>().enabled = false;
         }
     }
 }
